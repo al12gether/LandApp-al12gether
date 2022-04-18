844c2cb1d289cc7d5c2e2ef1c2a8937751f927e9
al12gether <alonetogetherpd@gmail.com>
2022-04-18 16:13:07 +0900

    방 상세 정보 화면 UI 추가

---
+++ app/src/main/java/com/example/landapp_al12gether/viewRoonDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.landapp_al12gether
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class viewRoonDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_roon_detail)
+    }
+}
